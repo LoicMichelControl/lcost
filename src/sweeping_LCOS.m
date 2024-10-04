0233880d211d9b71a2fa5a978ad03db1731d807c
Loïc MICHEL <89456326+LoicMichelControl@users.noreply.github.com>
2024-10-04 21:12:52 +0200

    update to v1.8.2 (file cleanup)

--- src/sweeping_LCOS.m
+++ src/sweeping_LCOS.m
@@ -1,61 +1,33 @@
 
-clear all
-path = '../Examples/OpenLoop/';
-RUN_LiftControl_Stabilization_v1_8_2;
+% Run iteratively the updated examples for v1.8.2
+% -> comment 'clear all' in the main LCO/St file 
+% -> comment 'Tmax' in the main LCO/St file 
+% -> comment every example path in the main LCO/St file 
+% -> check the name of the zip file (for Turbsim files) in the main LCO/st file
 
-clear all
-path = '../Examples/Lift_pitch_15d/';
-RUN_LiftControl_Stabilization_v1_8_2;
-
-clear all
-path = '../Examples/Lift_pitch_15d_sto13/';
-RUN_LiftControl_Stabilization_v1_8_2;
-
-clear all
-path = '../Examples/Lift_pitch_15d_sto18/';
-RUN_LiftControl_Stabilization_v1_8_2;
-
-clear all
-path = '../Examples/Lift_pitch_15d_13sto_MPPT/';
-RUN_LiftControl_Stabilization_v1_8_2;
-
- 
 clear all
 TMax = 2800;  
 path = '../Examples/Lift_pitch_15d_18sto_MPPT/';
 RUN_LiftControl_Stabilization_v1_8_2;
- 
 
+clear all
+TMax = 3000; 
+path = '../Examples/Lift_pitch_15d_13sto_MPPT/';
+RUN_LiftControl_Stabilization_v1_8_2;
 
+clear all
+TMax = 3000; 
+path = '../Examples/Lift_pitch_15d_sto13/';
+RUN_LiftControl_Stabilization_v1_8_2;
 
-%clear all
-%path = '../Examples/Lift_1/';
-%RUN_LiftControl_Stabilization_v1_8;
+clear all
+TMax = 3000; 
+path = '../Examples/OpenLoop/';
+RUN_LiftControl_Stabilization_v1_8_2;
 
-%clear all
-%path = '../Examples/Lift_2/';
-%RUN_LiftControl_Stabilization_v1_8;
+clear all
+TMax = 3000; 
+path = '../Examples/Lift_pitch_15d/';
+RUN_LiftControl_Stabilization_v1_8_2;
 
-%clear all
-%path = '../Examples/RBM_1/';
-%RUN_LiftControl_Stabilization_v1_8;
-
-%clear all
-%path = '../Examples/RBM_2/';
-%RUN_LiftControl_Stabilization_v1_8;
-
-%clear all
-%path = '../Examples/LiftIncremental_1/';
-%RUN_LiftControl_Stabilization_v1_8;
-
-%clear all
-%path = '../Examples/LiftIncremental_2/';
-%RUN_LiftControl_Stabilization_v1_8;
-
-%clear all
-%path = '../Examples/IPC_control/';
-%RUN_LiftControl_Stabilization_v1_8;
-
-%clear all
-%path = '../Examples/ComputeSurface/';
-%RUN_LiftControl_Stabilization_v1_8;
+%%% END
